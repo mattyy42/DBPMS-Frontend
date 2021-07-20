bc63cf273e6f340c4d799d708661febaeaf32cc3
Misker Birhanu <miskerbirhanu@gmail.com>
2021-07-20 15:13:10 +0300

    commit 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Applicant/Header';
+import Sidebar from './Applicant/Sidebar';
+import Dashboard from './Applicant/Dashboard'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React dbpms woooooooooooooooooooo
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <Sidebar />
+      <Dashboard />
     </div>
   );
 }
