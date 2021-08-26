457411471ace71fa052bce638d4ef2dc53d7fea0
mattyy42 <mikim2761@gmail.com>
2021-08-26 08:40:25 +0300

    updated

--- src/Applicant/Header.js
+++ src/Applicant/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
                     </ul>
                     {/* Right navbar links */}
                     <ul className="navbar-nav ml-auto">
-                        {/* Navbar Search */}
+                       
                         <li className="nav-item">
 
                             <div className="navbar-search-block">
@@ -52,90 +52,9 @@ class Header extends Component {
                             </div>
                         </li>
                         {/* Messages Dropdown Menu */}
-                        <li className="nav-item dropdown">
-                            <a href="#drop_" className="nav-link" data-toggle="dropdown">
-                                <i className="far fa-comments" />
-                                <span className="badge badge-danger navbar-badge">3</span>
-                            </a>
-                            <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                                <a href="#dorp-item" className="dropdown-item">
-                                    {/* Message Start */}
-                                    <div className="media">
-                                        <img src="dist/img/user1-128x128.jpg" alt="User Avatar" className="img-size-50 mr-3 img-circle" />
-                                        <div className="media-body">
-                                            <h3 className="dropdown-item-title">
-                                                Brad Diesel
-                  <span className="float-right text-sm text-danger"><i className="fas fa-star" /></span>
-                                            </h3>
-                                            {/* <p className="text-sm">{this.props.history.data.notif}ddd</p> */}
-                                            <p className="text-sm text-muted"><i className="far fa-clock mr-1" /> 4 Hours Ago</p>
-                                        </div>
-                                    </div>
-                                    {/* Message End */}
-                                </a>
-                                <div className="dropdown-divider" />
-                                <a href="#media" className="dropdown-item">
-                                    {/* Message Start */}
-                                    <div className="media">
-                                        <img src="dist/img/user8-128x128.jpg" alt="User Avatar" className="img-size-50 img-circle mr-3" />
-                                        <div className="media-body">
-                                            <h3 className="dropdown-item-title">
-                                                John Pierce
-                  <span className="float-right text-sm text-muted"><i className="fas fa-star" /></span>
-                                            </h3>
-                                            <p className="text-sm">I got your message bro</p>
-                                            <p className="text-sm text-muted"><i className="far fa-clock mr-1" /> 4 Hours Ago</p>
-                                        </div>
-                                    </div>
-                                    {/* Message End */}
-                                </a>
-                                <div className="dropdown-divider" />
-                                <a href="#medi1" className="dropdown-item">
-                                    {/* Message Start */}
-                                    <div className="media">
-                                        <img src="dist/img/user3-128x128.jpg" alt="User Avatar" className="img-size-50 img-circle mr-3" />
-                                        <div className="media-body">
-                                            <h3 className="dropdown-item-title">
-                                                Nora Silvester
-                  <span className="float-right text-sm text-warning"><i className="fas fa-star" /></span>
-                                            </h3>
-                                            <p className="text-sm">The subject goes here</p>
-                                            <p className="text-sm text-muted"><i className="far fa-clock mr-1" /> 4 Hours Ago</p>
-                                        </div>
-                                    </div>
-                                    {/* Message End */}
-                                </a>
-                                <div className="dropdown-divider" />
-                                <a href="#see" className="dropdown-item dropdown-footer">See All Comments</a>
-                            </div>
-                        </li>
+                        
                         {/* Notifications Dropdown Menu */}
-                        <li className="nav-item dropdown">
-                            <a className="nav-link" data-toggle="dropdown" href="#bell">
-                                <i className="far fa-bell" />
-                                <span className="badge badge-warning navbar-badge">15</span>
-                            </a>
-                            <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                                <span className="dropdown-item dropdown-header">15 Notifications</span>
-                                <div className="dropdown-divider" />
-                                <a href="#4" className="dropdown-item">
-                                    <i className="fas fa-envelope mr-2" /> 4 new messages
-            <span className="float-right text-muted text-sm">3 mins</span>
-                                </a>
-                                <div className="dropdown-divider" />
-                                <a href="#friend" className="dropdown-item">
-                                    <i className="fas fa-users mr-2" /> 8 friend requests
-            <span className="float-right text-muted text-sm">12 hours</span>
-                                </a>
-                                <div className="dropdown-divider" />
-                                <a href="#report" className="dropdown-item">
-                                    <i className="fas fa-file mr-2" /> 3 new reports
-            <span className="float-right text-muted text-sm">2 days</span>
-                                </a>
-                                <div className="dropdown-divider" />
-                                <a href="#seeAll" className="dropdown-item dropdown-footer">See All Notifications</a>
-                            </div>
-                        </li>
+                        
                         <li className="nav-item">
                             <a className="nav-link" href="#button" role="button" type="submit" onClick={this.handleLogout}>
                                 <i className="fas fa-sign-out-alt" />
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -8,7 +8,7 @@ class LandingPage extends Component {
     render() {
        // const { identifier,Post} = this.state;
         return (
-            <div>
+            <div className="main-layout">
                 <div className="loader_bg">
                     <div className="loader"><img src="images/loading.gif" alt="#" /></div>
                 </div>
@@ -75,7 +75,7 @@ class LandingPage extends Component {
                             </div>
                             <div className="col-md-7">
                                 <div className="text-img">
-                                    <figure><img src="style.images/img.png" /></figure>
+                                    <figure><img src="images/img.png" /></figure>
                                 </div>
                             </div>
                         </div>
